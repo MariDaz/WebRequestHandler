d596ae7f38fa1688ce9a9a40cd343267bf815dcb
Maria de los Angeles <92822454+MariDaz@users.noreply.github.com>
2024-09-08 09:41:24 +0000

    Sitio Web Dinamico Agregue el diccionario contenido para enviar en cuanto des clic al link a la direccion

--- web.py
+++ web.py
@@ -1,13 +1,32 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
 
+contenido = {
+    '/proyecto/1': """
+<html>
+  <h1>Proyecto: Web Estática - App de recomendación de libros</h1>
+  <p>Esta aplicación recomienda libros basándose en tus preferencias.</p>
+</html>""",
+    '/proyecto/2': """
+<html>
+  <h1>Proyecto: Web App - MeFalta, qué película o serie me falta ver</h1>
+  <p>MeFalta te ayuda a seguir las películas o series que aún no has visto.</p>
+</html>""",
+    '/proyecto/3': """
+<html>
+  <h1>Proyecto: Web App - Foto22, web para gestión de fotos</h1>
+  <p>Foto22 es una aplicación para gestionar tus fotos en la nube.</p>
+</html>"""
+}
 
 class WebRequestHandler(BaseHTTPRequestHandler):
+
+
     def url(self):
         return urlparse(self.path)
 
-    #def query_data(self):
-     #   return dict(parse_qsl(self.url().query))
+    def query_data(self):
+      return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
         if self.path=="/":
@@ -22,8 +41,18 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             except FileNotFoundError:
                 # Si no encontramos el archivo home.html, devolvemos un error
                 self.wfile.write(b"<h1>Error: Archivo home.html no encontrado</h1>")
+        
+        elif self.path in contenido:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(contenido[self.path].encode("utf-8"))
+
+        # Si la ruta no es reconocida, devolvemos un error 404
         else:
-            self.send_error(404,"Pagina no encontrada")
+            self.send_error(404, "Pagina no encontrada")
+
+
 
     def get_response(self):
 
