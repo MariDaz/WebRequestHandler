57f56f727f4947b40c4eeae1cd68db904d935875
Maria de los Angeles <92822454+MariDaz@users.noreply.github.com>
2024-09-08 08:34:17 +0000

    Home Page Creo un if que si la ruta es "/" nos lleva a la pagina home.html, y en caso contrario nos lanza un error 404

--- web.py
+++ web.py
@@ -6,16 +6,26 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def url(self):
         return urlparse(self.path)
 
-    def query_data(self):
-        return dict(parse_qsl(self.url().query))
+    #def query_data(self):
+     #   return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        if self.path=="/":
+            self.send_response(200)
+            self.send_header("Content-Type","text/html") #se indica que es html
+            self.end_headers()
 
-     def get_response(self):
+            try:
+                with open("home.html", "r", encoding="utf-8") as file:
+                    content = file.read()  # Leer el contenido del archivo
+                    self.wfile.write(content.encode("utf-8"))  # Escribirlo en la respuesta
+            except FileNotFoundError:
+                # Si no encontramos el archivo home.html, devolvemos un error
+                self.wfile.write(b"<h1>Error: Archivo home.html no encontrado</h1>")
+        else:
+            self.send_error(404,"Pagina no encontrada")
+
+    def get_response(self):
 
         path=self.url().path #ruta URL
         query_data=self.query_data() # parametros de la query string
